425a17e30a3f9120b66ead467bb0854e47286517
gakrchintugmailcom <124022186+gakrchintugmailcom@users.noreply.github.com>
2026-05-13 17:40:06 +0100

    Delete sre-hiring-exercise-main/sre-hiring-exercise-main directory

--- sre-hiring-exercise-main/sre-hiring-exercise-main/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "app_role_arn" {
-  description = "ARN of the application IAM role"
-  value       = aws_iam_role.app.arn
-}
-
-output "artefact_bucket_name" {
-  description = "Name of the S3 artefact bucket"
-  value       = aws_s3_bucket.artefacts.bucket
-}
